07a1ac3c8719733d8bcb2579196d5906fde4eb98
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-16 16:24:25 +0100

    fix: ease how modules are resolved

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const heroiconsPlugin: IconsaucePlugin = {
     lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
   },
   selector: (libPath: RegExpMatchArray) => `hero/${libPath[2].replace(/[_]+/g, '-')}`,
-  path: `${resolve('node_modules', 'heroicons')}/solid/**/*.svg`,
+  path: `${resolve(__dirname, '../../../heroicons')}/solid/**/*.svg`,
 }
 
 export default heroiconsPlugin
